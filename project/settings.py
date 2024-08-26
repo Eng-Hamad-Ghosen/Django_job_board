81cd3b665069204726a3e98b5d0598e41768c606
Hamad <hamad.sa.ghosen@gmail.com>
2024-08-26 17:41:32 +0300

    Small Edit

--- project/settings.py
+++ project/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_ROOT='media/'
+MEDIA_URL='media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 
 STATICFILES_DIRS=[
     BASE_DIR / "static",
